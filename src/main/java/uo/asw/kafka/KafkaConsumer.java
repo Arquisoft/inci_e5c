f2e1e294800c454d2197574c6a975885d382df9a
Miguel <miguelvilla997@gmail.com>
2018-06-24 19:30:30 +0200

    Cambio menor en el KafkaConsumer para que haga build

--- src/main/java/uo/asw/kafka/KafkaConsumer.java
+++ src/main/java/uo/asw/kafka/KafkaConsumer.java
@@ -5,8 +5,6 @@ import java.io.IOException;
 
 import javax.annotation.ManagedBean;
 
-import org.json.JSONException;
-import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.kafka.annotation.KafkaListener;
